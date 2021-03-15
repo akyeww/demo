301925e2891fa41d83dfad018aec1d608b0e22d1
akyeww <56905355+akyeww@users.noreply.github.com>
2021-03-15 16:30:43 +0900

    Add files via upload

--- filter/assets/script.js
+++ filter/assets/script.js
@@ -71,54 +71,57 @@ const addFilter = async () => {
     canvasCtx.restore();
 }
 
+let filterMode = 0;
 function filterImage() {
     let imgWidth = canvasElement.width;
     let imgHeight = canvasElement.height;
 
     canvasCtx.drawImage(video, 0, 0, imgWidth, imgHeight);
 
+    if (filterMode === 0) return;
+
     let imageData = canvasCtx.getImageData(0, 0, imgWidth, imgHeight);
 
-    // Grayscale
-    // Same as: canvasElement.style.filter = "grayscale(100%)";
-    if (false) {
-        for (let i = 0; i < imageData.data.length; i += 4) {
-            let r = imageData.data[i];
-            let g = imageData.data[i + 1];
-            let b = imageData.data[i + 2];
-            let averageColour = (r + g + b) / 3;
-            imageData.data[i] = averageColour;
-            imageData.data[i + 1] = averageColour;
-            imageData.data[i + 2] = averageColour;
-        }
-    }
-
-    // Inverted colours
-    // Same as: canvasElement.style.filter = "invert(100%)";
-    if (false) {
-        for (let i = 0; i < imageData.data.length; i += 4) {
-            let r = imageData.data[i];
-            let g = imageData.data[i + 1];
-            let b = imageData.data[i + 2];
-            imageData.data[i] = 255 - r;
-            imageData.data[i + 1] = 255 - g;
-            imageData.data[i + 2] = 255 - b;
-        }
-    }
-
-    // Complementary colours
-    // Same as: canvasElement.style.filter = "hue-rotate(180deg)";
-    if (false) {
-        for (let i = 0; i < imageData.data.length; i += 4) {
-            let r = imageData.data[i];
-            let g = imageData.data[i + 1];
-            let b = imageData.data[i + 2];
-            let sum = Math.max(r, g, b) + Math.min(r, g, b);
-
-            imageData.data[i] = sum - r;
-            imageData.data[i + 1] = sum - g;
-            imageData.data[i + 2] = sum - b;
-        }
+    switch (filterMode) {
+        case 1:
+            // Grayscale
+            // Same as: canvasElement.style.filter = "grayscale(100%)";
+            for (let i = 0; i < imageData.data.length; i += 4) {
+                let r = imageData.data[i];
+                let g = imageData.data[i + 1];
+                let b = imageData.data[i + 2];
+                let averageColour = (r + g + b) / 3;
+                imageData.data[i] = averageColour;
+                imageData.data[i + 1] = averageColour;
+                imageData.data[i + 2] = averageColour;
+            }
+            break;
+        case 2:
+            // Inverted colours
+            // Same as: canvasElement.style.filter = "invert(100%)";
+            for (let i = 0; i < imageData.data.length; i += 4) {
+                let r = imageData.data[i];
+                let g = imageData.data[i + 1];
+                let b = imageData.data[i + 2];
+                imageData.data[i] = 255 - r;
+                imageData.data[i + 1] = 255 - g;
+                imageData.data[i + 2] = 255 - b;
+            }
+            break;
+        case 3:
+            // Complementary colours
+            // Same as: canvasElement.style.filter = "hue-rotate(180deg)";
+            for (let i = 0; i < imageData.data.length; i += 4) {
+                let r = imageData.data[i];
+                let g = imageData.data[i + 1];
+                let b = imageData.data[i + 2];
+                let sum = Math.max(r, g, b) + Math.min(r, g, b);
+        
+                imageData.data[i] = sum - r;
+                imageData.data[i + 1] = sum - g;
+                imageData.data[i + 2] = sum - b;
+            }
+            break;
     }
 
     canvasCtx.putImageData(imageData, 0, 0);
@@ -137,6 +140,27 @@ function resizeCanvas() {
     // canvasElement.style.height = window.innerHeight + 'px';
 }
 
+function nextFilter() {
+    filterMode = (filterMode >= 3) ? 0 : filterMode + 1;
+
+    let mode = '';
+    switch (filterMode) {
+        case 0:
+            mode = 'None';
+            break;
+        case 1:
+            mode = 'Grayscale';
+            break;
+        case 2:
+            mode = 'Inverted';
+            break;
+        case 3:
+            mode = 'Complementary';
+            break;
+    }
+    document.querySelector('.filter-button').innerHTML = `Filter: ${mode}`;
+}
+
 function pauseVideo() {
     const button = document.querySelector('.stop-button');
     if (video.paused) {
@@ -153,9 +177,13 @@ function addListener() {
     // video.addEventListener('loadedmetadata', resizeCanvas);
     // videoElement.addEventListener('play', resizeCanvas);
 
+    /* NEXT BUTTON */
+    const filterButton = document.querySelector('.filter-button');
+    filterButton.addEventListener('click', nextFilter);
+
     /* PAUSE BUTTON */
-    const button = document.querySelector('.stop-button');
-    button.addEventListener('click', pauseVideo);
+    const stopButton = document.querySelector('.stop-button');
+    stopButton.addEventListener('click', pauseVideo);
 
     document.addEventListener('keyup', logKey);
     function logKey(e) {
